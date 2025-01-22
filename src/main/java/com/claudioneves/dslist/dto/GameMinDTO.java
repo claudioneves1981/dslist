739f9cab3e69f3a9421f56a7febed4183f032b2e
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2025-01-22 17:52:59 -0300

    SQL, Projection, GET games by list

--- src/main/java/com/claudioneves/dslist/dto/GameMinDTO.java
+++ src/main/java/com/claudioneves/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.claudioneves.dslist.dto;
 
 import com.claudioneves.dslist.entities.Game;
+import com.claudioneves.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -20,6 +21,14 @@ public class GameMinDTO {
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
--- src/main/java/com/claudioneves/dslist/services/GameService.java
+++ src/main/java/com/claudioneves/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.claudioneves.dslist.services;
 import com.claudioneves.dslist.dto.GameDTO;
 import com.claudioneves.dslist.dto.GameMinDTO;
 import com.claudioneves.dslist.entities.Game;
+import com.claudioneves.dslist.projections.GameMinProjection;
 import com.claudioneves.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,4 +30,12 @@ public class GameService {
        List<Game> result =  gameRepository.findAll();
        return result.stream().map(GameMinDTO::new).toList();
     }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result =  gameRepository.searchByList(listId);
+        return result.stream().map(GameMinDTO::new).toList();
+    }
+
+
 }
