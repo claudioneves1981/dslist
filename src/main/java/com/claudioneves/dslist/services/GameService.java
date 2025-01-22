649d6c3d1624f9d07c2c82c8ea108932f97b9310
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2025-01-22 11:23:00 -0300

    GET game by id

--- src/main/java/com/claudioneves/dslist/services/GameService.java
+++ src/main/java/com/claudioneves/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.claudioneves.dslist.services;
 
+import com.claudioneves.dslist.dto.GameDTO;
 import com.claudioneves.dslist.dto.GameMinDTO;
 import com.claudioneves.dslist.entities.Game;
 import com.claudioneves.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,10 +17,16 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
-
        List<Game> result =  gameRepository.findAll();
-        return result.stream().map(GameMinDTO::new).toList();
-
+       return result.stream().map(GameMinDTO::new).toList();
     }
 }
